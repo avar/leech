6ef7bfad181e01c6e81d56e4e810058268535ddd
Hinrik Örn Sigurðsson <hinrik.sig@gmail.com>
2010-02-27 05:24:11 +0000

    Remove munin stuff, we keep track of that elsewhere

--- setup.sh
+++ setup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 LEECH="/home/leech"
-RTOM="rtom-0.2.0-080619"
-RTOM_FILE="$RTOM.tar.gz"
-RTOM_URL="http://projects.cyla.homeip.net/rtwi/downloads/rtom/$RTOM_FILE"
 
 # rtorrent dirs
 mkdir -p $LEECH/torrent/{complete,incomplete,queue,session}
@@ -11,14 +8,6 @@ mkdir -p $LEECH/torrent/{complete,incomplete,queue,session}
 ln -s $LEECH/torrent/complete   $LEECH/c
 ln -s $LEECH/torrent/incomplete $LEECH/i
 ln -s $LEECH/torrent/queue      $LEECH/q
-
-# munin plugins to monitor rtorrent
-wget -c -O /tmp/$RTOM_FILE $RTOM_URL
-mkdir $LEECH/torrent/rtom
-cd $LEECH/torrent/rtom
-tar zxf /tmp/$RTOM_FILE
-mv $RTOM rtom
-cd -
 
 # Link dotfiles
 if type link-files >/dev/null; then
